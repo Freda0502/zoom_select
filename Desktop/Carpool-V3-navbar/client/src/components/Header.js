215d5a3742e92e2d72515e3042ae26b581adf8fe
Freda0502 <68989706+Freda0502@users.noreply.github.com>
2021-07-07 17:32:19 -0500

    added in onlick to allow automatic refresh

--- Desktop/Carpool-V3-navbar/client/src/components/Header.js
+++ Desktop/Carpool-V3-navbar/client/src/components/Header.js
@@ -1,52 +1,48 @@
-import PropTypes from 'prop-types';
-import React from 'react';
-import styled from 'styled-components';
+import PropTypes from 'prop-types'
+import React from 'react'
+import styled from 'styled-components'
 
 const Heading = styled.h1`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    font-family: Josefin Sans;
-    font-style: normal;
-    font-weight: normal;
-    font-size: 48px;
-    line-height: 48px;
-
-    color: #002140;
-`;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: Josefin Sans;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 48px;
+  line-height: 48px;
+  color: #002140;
+`
 
 const Subheading = styled.h2`
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: Josefin Sans;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 20px;
+  line-height: 20px;
+  color: #bbdaff;
+`
 
-    font-family: Josefin Sans;
-    font-style: normal;
-    font-weight: normal;
-    font-size: 20px;
-    line-height: 20px;
-
-    color: #BBDAFF;
-`;
-
-const Header = ({title, subtitle}) => {
-    return (
-        <header className='header'>
-            <Heading>{title} </Heading>
-            <Subheading>{subtitle}</Subheading>
-        </header>
-    )
+const Header = ({ title, subtitle }) => {
+  return (
+    <header className='header'>
+      <Heading>{title} </Heading>
+      <Subheading>{subtitle}</Subheading>
+    </header>
+  )
 }
 
 Header.defaultProps = {
-    title: 'Rice Carpool',
-    subtitle: ''
+  title: 'Rice Carpool',
+  subtitle: '',
 }
 
 Header.propTypes = {
-    title: PropTypes.string, 
-    subtitle: PropTypes.string
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
 }
 
-export default Header; 
+export default Header
